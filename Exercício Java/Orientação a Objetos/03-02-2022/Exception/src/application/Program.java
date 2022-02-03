d2938737290c79b3cba4d45d10853df001621e17
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-02-03 18:12:45 -0300

    exeception update

--- Exercício Java/Orientação a Objetos/03-02-2022/Exception/src/application/Program.java
+++ Exercício Java/Orientação a Objetos/03-02-2022/Exception/src/application/Program.java
@@ -6,22 +6,19 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int idade = 0;
-		while(idade != -99) {
-			System.out.print("Entre a sua idade: ");
-			try {//tente
+		try {
+			do {
+				System.out.print("Entre a sua idade: ");
 				idade = sc.nextInt();
-				
+
 				System.out.println(idade);
-				
-			}
-			catch (InputMismatchException e) {//pegue
-				System.out.println("ERRO! Por favor, digite um número!");
-			}
-			finally {//finalize
-				System.out.println("Estamos no finally");
-			}
+
+			} while (idade > 1);
+		} catch (InputMismatchException e) {// Peguei
+			System.out.println("ERRO! Por favor, digite um número!");
 		}
+		sc.close();
 	}
 }
